2400df53cfbeb9555be06097ecdbe1da6798d741
dosherm <dans.home.23@gmail.com>
2025-11-21 14:59:40 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,35 @@
 
-const CACHE = "wc-v2";
+const CACHE = "wc-v3";
 const ASSETS = [
   "/winter_classic/index.html",
   "/winter_classic/manifest.webmanifest",
   "/winter_classic/assets/styles.css",
   "/winter_classic/assets/icon-192.png",
   "/winter_classic/assets/icon-512.png",
-  "/winter_classic/assets/maskable-512.png",
-  "/winter_classic/pages/white-net-best-ball.html",
-  "/winter_classic/pages/blue-net-best-ball.html",
-  "/winter_classic/pages/interflight-chapman.html",
-  "/winter_classic/pages/white-net-alt-best-ball.html",
-  "/winter_classic/pages/blue-net-alt-best-ball.html",
-  "/winter_classic/pages/interflight-scramble.html",
-  "/winter_classic/pages/white-singles.html",
-  "/winter_classic/pages/blue-singles.html",
-  "/winter_classic/pages/interflight-singles.html"
+  "/winter_classic/assets/maskable-512.png"
 ];
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k!==CACHE).map(k => caches.delete(k))))
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
   );
 });
-self.addEventListener("fetch", e => {
-  const url = new URL(e.request.url);
-  if (ASSETS.includes(url.pathname)) {
-    e.respondWith(caches.match(e.request));
-  } else {
-    e.respondWith(
-      caches.match(e.request).then(res => res || fetch(e.request).then(resp => {
-        const copy = resp.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
-        return resp;
-      }).catch(() => caches.match("/winter_classic/index.html")))
-    );
-  }
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
